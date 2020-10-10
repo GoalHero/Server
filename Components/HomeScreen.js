6a2b4ee7b5a246460b9d3b6d59b38b61bfd8d647
Danielle Arquisola <64483418+daniellearquisola@users.noreply.github.com>
2020-10-10 14:33:17 -0500

    chore: added dummy text to home page

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -4,7 +4,11 @@ import { View, Text, Button, StyleSheet, StatusBar } from "react-native";
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text>
+        Welcome to Goal Hero! {"\n\n"}
+        Blah blah blah. How does goal hero work? {"\n\n"}
+        Make goals for yourself. Level up and defeat the enemy.
+      </Text>
     </View>
   );
 };
