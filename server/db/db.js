48ce0fab96037902a43b39b3f7fd974671264086
ericfish12 <ericfish1221@gmail.com>
2021-04-09 15:13:54 -0400

    pg config

--- server/db/db.js
+++ server/db/db.js
@@ -6,6 +6,7 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
+    ssl: { rejectUnauthorized: false } 
   }
 );
 module.exports = db;
